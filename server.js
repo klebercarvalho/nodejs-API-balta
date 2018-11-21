a6113fb9c9af7f873292f71614d29a1ad4648e48
Kleber Carvalho <kleber.carvalho@gmail.com>
2018-11-20 22:28:11 -0200

    Starting debug

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const app = express();
 const port = normalizePort(process.env.PORT || 3000);
 app.set("port", port);
 
-
 const server = http.createServer(app);
 const router = express.Router();
 
@@ -21,7 +20,8 @@ const route = router.get("/", (req, res, next) => {
 app.use("/", route);
 
 server.listen(port);
-server.on('error', onError);
+server.on("error", onError);
+server.on("listing", onListing);
 console.log("API rodando na porta " + port);
 
 function normalizePort(val) {
@@ -35,24 +35,28 @@ function normalizePort(val) {
 }
 
 function onError(error) {
-    if (error.syscall !== 'listen') {
-      throw error;
-    }
-  
-    const bind = typeof port === 'string'
-      ? 'Pipe ' + port
-      : 'Port ' + port;
-  
-    switch (error.code) {
-      case 'EACCES':
-        console.error(bind + ' requires elevated privileges');
-        process.exit(1);
-        break;
-      case 'EADDRINUSE':
-        console.error(bind + ' is already in use');
-        process.exit(1);
-        break;
-      default:
-        throw error;
-    }
+  if (error.syscall !== "listen") {
+    throw error;
   }
+
+  const bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
+
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+function onListening() {
+  const addr = server.address();
+  const bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
+  debug("Listening on " + bind);
+}
