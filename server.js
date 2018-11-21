c1df25852c63cb20eef51a7586c731a168f2ce41
Kleber Carvalho <kleber.carvalho@gmail.com>
2018-11-20 22:21:49 -0200

    Managing server error

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const app = express();
 const port = normalizePort(process.env.PORT || 3000);
 app.set("port", port);
 
+
 const server = http.createServer(app);
 const router = express.Router();
 
@@ -20,6 +21,7 @@ const route = router.get("/", (req, res, next) => {
 app.use("/", route);
 
 server.listen(port);
+server.on('error', onError);
 console.log("API rodando na porta " + port);
 
 function normalizePort(val) {
@@ -31,3 +33,26 @@ function normalizePort(val) {
 
   return false;
 }
+
+function onError(error) {
+    if (error.syscall !== 'listen') {
+      throw error;
+    }
+  
+    const bind = typeof port === 'string'
+      ? 'Pipe ' + port
+      : 'Port ' + port;
+  
+    switch (error.code) {
+      case 'EACCES':
+        console.error(bind + ' requires elevated privileges');
+        process.exit(1);
+        break;
+      case 'EADDRINUSE':
+        console.error(bind + ' is already in use');
+        process.exit(1);
+        break;
+      default:
+        throw error;
+    }
+  }
