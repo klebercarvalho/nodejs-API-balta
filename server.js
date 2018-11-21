60b081a118d8af4317ad4ea1c56410311e5f1f93
Kleber Carvalho <kleber.carvalho@gmail.com>
2018-11-20 22:11:00 -0200

    Normalizing server port number

--- server.js
+++ server.js
@@ -1,24 +1,33 @@
-'use strict'
+"use strict";
 
-const http = require('http');
-const debug = require('debug')('nodestr:server');
-const express = require('express');
+const http = require("http");
+const debug = require("debug")("nodestr:server");
+const express = require("express");
 
 const app = express();
-const port = 3000;
-app.set('port', port);
-
+const port = normalizePort(process.env.PORT || 3000);
+app.set("port", port);
 
 const server = http.createServer(app);
 const router = express.Router();
 
-const route = router.get('/', (req, res, next) => {
-    res.status(200).send( {
-        title: "Node Store API",
-        version: "0.0.1"
-    });
+const route = router.get("/", (req, res, next) => {
+  res.status(200).send({
+    title: "Node Store API",
+    version: "0.0.1"
+  });
 });
-app.use('/', route);
+app.use("/", route);
 
 server.listen(port);
-console.log('API rodando na porta ' + port);
+console.log("API rodando na porta " + port);
+
+function normalizePort(val) {
+  const port = parseInt(val, 10);
+
+  if (port >= 0) {
+    return port;
+  }
+
+  return false;
+}
