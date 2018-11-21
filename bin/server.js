ee252cd83d9942eeddf3443eecea9f8daeb5269a
Kleber Carvalho <kleber.carvalho@gmail.com>
2018-11-21 00:08:57 -0200

    Splitting server and app

--- bin/server.js
+++ bin/server.js
@@ -1,27 +1,17 @@
 "use strict";
 
+const app = require("../src/app");
+const debug = require("debug")("balta:server");
 const http = require("http");
-const debug = require("debug")("nodestr:server");
-const express = require("express");
 
-const app = express();
 const port = normalizePort(process.env.PORT || 3000);
 app.set("port", port);
 
 const server = http.createServer(app);
-const router = express.Router();
-
-const route = router.get("/", (req, res, next) => {
-  res.status(200).send({
-    title: "Node Store API",
-    version: "0.0.1"
-  });
-});
-app.use("/", route);
 
 server.listen(port);
 server.on("error", onError);
-server.on("listing", onListing);
+server.on("listing", onListening);
 console.log("API rodando na porta " + port);
 
 function normalizePort(val) {
