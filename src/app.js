8dd8c0ed29edabd17e764985eefa8b38a7f01298
Kleber Carvalho <kleber.carvalho@gmail.com>
2018-11-22 00:20:18 -0200

    routes

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
-"use strict";
-
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
-const router = express.Router();
+
+// load route
+const index = require('./routes/index');
+const product = require('./routes/product');
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -12,36 +13,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-const route = router.get("/", (req, res, next) => {
-  res.status(200).send({
-    title: "Node Store API",
-    version: "0.0.1"
-  });
-});
-
-const create = router.post("/", (req, res, next) => {
-  res.status(201).send(req.body);
-});
-
-const put = router.put("/:id", (req, res, next) => {
-  const id = req.params.id;
-  res.status(200).send({
-    id: id,
-    item: req.body
-  });
-});
-
-const del = router.delete("/:id", (req, res, next) => {
-  const id = req.params.id;
-  res.status(200).send({
-    id: id,
-    item: req.body
-  });
-});
-
-app.use("/", route);
-app.use("/product", create);
-app.use("/product", put);
-app.use("/product", del);
+app.use('/', index);
+app.use('/product', product);
 
 module.exports = app;
