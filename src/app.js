3c49ea2791e03396519711a7006049dba167c2a3
Kleber Carvalho <kleber.carvalho@gmail.com>
2018-11-23 17:45:49 -0200

    creating a product

--- src/app.js
+++ src/app.js
@@ -7,6 +7,9 @@ const app = express();
 // connect to DB
 mongoose.connect('mongodb://127.0.0.1:27017/api-balta');
 
+// load models
+const Product = require('./models/product');
+
 // load route
 const index = require('./routes/index').default;
 const product = require('./routes/product');
