d1c8f8390062f911f007ae024cefbf59c8225422
Kleber Carvalho <kleber.carvalho@gmail.com>
2018-11-29 20:56:11 -0200

    put find by id and update

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -3,11 +3,12 @@ const mongoose = require('mongoose');
 const Product = mongoose.model('Product');
 
 exports.get = (req, res, next) => {
-  Product
-    .find({
+  Product.find(
+    {
       active: true,
     },
-    'title price slug')
+    'title price slug',
+  )
     .then((data) => {
       res.status(200).send(data);
     })
@@ -17,40 +18,67 @@ exports.get = (req, res, next) => {
 };
 
 exports.getBySlug = (req, res, next) => {
-  Product
-    .findOne({
+  Product.findOne(
+    {
       slug: req.params.slug,
       active: true,
     },
-    'title description price slug tags')
+    'title description price slug tags',
+  )
     .then((data) => {
       res.status(200).send(data);
-    }).catch((e) => {
+    })
+    .catch((e) => {
       res.status(400).send(e);
     });
 };
 
 exports.getByTag = (req, res, next) => {
-  Product
-    .find({
+  Product.find(
+    {
       tags: req.params.tag,
       active: true,
     },
-    'title description price slug tags')
+    'title description price slug tags',
+  )
     .then((data) => {
       res.status(200).send(data);
-    }).catch((e) => {
+    })
+    .catch((e) => {
       res.status(400).send(e);
     });
 };
 
 exports.getById = (req, res, next) => {
-  Product
-    .findById(req.params.id)
+  Product.findById(req.params.id)
     .then((data) => {
       res.status(200).send(data);
-    }).catch((e) => {
+    })
+    .catch((e) => {
       res.status(400).send(e);
+    });
+};
+
+exports.put = (req, res, next) => {
+  Product
+    .findByIdAndUpdate(req.params.id, {
+      $set: {
+        title: req.body.title,
+        description: req.body.description,
+        price: req.body.price,
+        slug: req.body.slug,
+      },
+    })
+    .then((x) => {
+      res.status(200).send({
+        message: 'Produto atualizado com sucesso!',
+      });
+    })
+    .catch((e) => {
+      res.status(400).send({
+        message: 'Falha ao atualizar produto',
+        data: e,
+      });
     });
 };
 
@@ -67,14 +95,6 @@ exports.post = (req, res, next) => {
         data: e,
       });
     });
-};
-
-exports.put = (req, res, next) => {
-  const { id } = req.params;
-  res.status(200).send({
-    id,
-    item: req.body,
-  });
 };
 
 exports.delete = (req, res, next) => {
